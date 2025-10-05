8db60c897930670650af5c9aeaf2e589f5be3346
Szymon Zubek <zubekszymon@gmail.com>
2025-10-05 18:22:40 +0100

    Updated open-github.sh script to use gh CLI

--- .config/scripts/open-github.sh
+++ .config/scripts/open-github.sh
@@ -1,14 +1,7 @@
-#!/bin/bash
-dir="${1:-$PWD}"
-cd "$dir" || exit 1
+CURRENT_DIR="$(pwd)"
 
-# Check if it's a git repo
-if git -C "$dir" rev-parse --is-inside-work-tree >/dev/null 2>&1; then
-    url=$(git -C "$dir" remote get-url origin 2>/dev/null)
-    if [[ $url =~ git@github.com:(.*).git ]]; then
-        https_url="https://github.com/${BASH_REMATCH[1]}"
-        open "$https_url"
-    fi
+if [[ -d "$CURRENT_DIR/.git" ]]; then
+	gh repo view --web
 else
-    echo "Not a git repo."
+	echo "Not a git repo."
 fi
