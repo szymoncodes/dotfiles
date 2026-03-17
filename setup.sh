fe9dbb72a6109da99db69a6ebb06079df9cff1b3
Szymon Zubek <me@szymonzubek.com>
2026-03-17 00:28:53 +0000

    Added setup.sh for devcontainers

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Setup starting..."
+echo "Brew installing..."
+brew install neovim --HEAD
+brew install fzf bat ripgrep stow starship tmux zsh-autosuggestions zsh-syntax-highlighting sk fd fastfetch gcc uv gh zsh
+echo "Brew finished."
+
+echo "Changing shell to zsh..."
+sudo usermod -s /bin/zsh vscode
+echo "Shell updated."
+
+echo "Stowing dotfiles..."
+stow --ignore=".zshrc" --ignore=".aerospace.toml" --ignore=".gitconfig" --ignore=".tmux.conf" ~/dotfiles
+
+echo "Setup finished."
