54dfd29626747435a2fd76cdfda7ce6e8a3e2053
Szymon Zubek <me@szymonzubek.com>
2026-03-17 23:44:23 +0000

    Fixed uv project setup to correct directory | Added go and npm to brew
    Signed-off-by: Szymon Zubek <me@szymonzubek.com>

--- setup.sh
+++ setup.sh
@@ -2,19 +2,21 @@
 echo "Setup starting..."
 
 echo "Brew installing..."
-brew install fzf bat ripgrep stow starship zsh-autosuggestions zsh-syntax-highlighting sk fd fastfetch gcc gh
+brew install fzf bat ripgrep stow starship zsh-autosuggestions zsh-syntax-highlighting sk fd fastfetch gcc gh go npm
 echo "Brew finished."
 
+
+echo "Stowing dotfiles..."
+cd && cd dotfiles && stow --ignore="bootstrap.sh" --ignore=".aerospace.toml" --ignore=".config/ghostty/config" --adopt . && git restore .
+
 echo "Setting up basic project structure..."
-uv init && uv venv && uv python install 3.14
+cd && cd .. && cd .. && cd workspaces && cd $(ls)
+uv init && uv venv && uv python install 3.14 && uv python update-shell
 mkdir -p src/${PWD##*/}
 mkdir docs
 mkdir data
 mkdir exports
 mv main.py src/${PWD##*/}/main.py
-
-echo "Stowing dotfiles..."
-cd && cd dotfiles && stow --ignore="bootstrap.sh" --ignore=".aerospace.toml" --ignore=".config/ghostty/config" --adopt . && git restore .
 
 echo "Cleaning home directory..."
 cd && rm -rf .bash_logout .bashrc .oh-my-zsh .zcom*
