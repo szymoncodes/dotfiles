f22f741e0ce7a2562122b4e28d8fa1bd1e466abf
Szymon Zubek <me@szymonzubek.com>
2026-03-17 23:00:52 +0000

    Expanded the script to create a project directory using uv and to remove unnecessary files from home dir
    Signed-off-by: Szymon Zubek <me@szymonzubek.com>

--- setup.sh
+++ setup.sh
@@ -5,9 +5,19 @@ echo "Brew installing..."
 brew install fzf bat ripgrep stow starship zsh-autosuggestions zsh-syntax-highlighting sk fd fastfetch gcc gh
 echo "Brew finished."
 
+echo "Setting up basic project structure..."
+uv init && uv venv && uv python install 3.14
+mkdir -p src/${PWD##*/}
+mkdir docs
+mkdir data
+mkdir exports
+mv main.py src/${PWD##*/}/main.py
+
 echo "Stowing dotfiles..."
 cd && cd dotfiles && stow --ignore="bootstrap.sh" --ignore=".aerospace.toml" --ignore=".config/ghostty/config" --adopt . && git restore .
-echo "Dotfiles symlinked."
+
+echo "Cleaning home directory..."
+cd && rm -rf .bash_logout .bashrc .oh-my-zsh .zcom*
 
 echo "Installing LSPs, Linters, Formatters with Mason..."
 nvim --headless -c "MasonInstall gopls lua-language-server prettier ruff stylua tinymist ty typstyle"
