822702a936b83aef0ded4db5358838a2d79f40ee
Szymon Zubek <me@szymonzubek.com>
2026-03-17 21:49:32 +0000

    Added script to install Mason packages automatically
    Signed-off-by: Szymon Zubek <me@szymonzubek.com>

--- setup.sh
+++ setup.sh
@@ -7,5 +7,9 @@ echo "Brew finished."
 
 echo "Stowing dotfiles..."
 cd && cd dotfiles && stow --ignore="bootstrap.sh" --ignore=".aerospace.toml" --ignore=".config/ghostty/config" --adopt . && git restore .
+echo "Dotfiles symlinked."
+
+echo "Installing LSPs, Linters, Formatters with Mason..."
+nvim --headless -c "MasonInstall gopls lua-language-server prettier ruff stylua tinymist ty typstyle"
 
 echo "Setup finished."
