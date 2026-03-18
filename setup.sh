f9dba6bb8dd47706d8eaf5b9711b8655d5adaffe
Szymon Zubek <me@szymonzubek.com>
2026-03-18 12:38:11 +0000

    Moved Mason installation back into setup.sh
    Signed-off-by: Szymon Zubek <me@szymonzubek.com>

--- setup.sh
+++ setup.sh
@@ -8,4 +8,7 @@ echo "Brew finished."
 echo "Stowing dotfiles..."
 cd && cd dotfiles && stow --ignore="bootstrap.sh" --ignore=".aerospace.toml" --ignore=".config/ghostty/config" --adopt . && git restore .
 
+echo "Installing LSPs, Linters, Formatters with Mason..."
+nvim --headless -c "MasonInstall gopls lua-language-server prettier ruff stylua tinymist ty typstyle"
+
 echo "Setup finished."
