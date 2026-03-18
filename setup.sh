52e3cf79d38eac0806256ac6473fd76fee4d06fb
Szymon Zubek <me@szymonzubek.com>
2026-03-18 21:02:37 +0000

    Went back to Homebrew | Made setup easier to reproduce
    Signed-off-by: Szymon Zubek <me@szymonzubek.com>

--- setup.sh
+++ setup.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 echo "Setup starting..."
 
+echo "Installing tools..."
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+packages=(
+  fzf
+  bat
+  fd
+  ripgrep
+  stow
+  starship
+  gcc
+  tmux
+  neovim --HEAD
+)
+
+for pkg in "${packages[@]}"; do
+  echo "Installing $pkg..."
+  brew install "$pkg"
+done
+
 echo "Stowing dotfiles..."
 cd && cd dotfiles && stow --ignore="bootstrap.sh" --ignore=".aerospace.toml" --ignore=".config/ghostty/config" --adopt . && git restore .
 
