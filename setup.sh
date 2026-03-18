7b4fa9d3b97ff3e31f56e735642e4fb9b5acf830
Szymon Zubek <me@szymonzubek.com>
2026-03-18 10:52:40 +0000

    Removed project specific settings
    Signed-off-by: Szymon Zubek <me@szymonzubek.com>

--- setup.sh
+++ setup.sh
@@ -5,23 +5,7 @@ echo "Brew installing..."
 brew install fzf bat ripgrep stow starship zsh-autosuggestions zsh-syntax-highlighting sk fd fastfetch gcc gh
 echo "Brew finished."
 
-
 echo "Stowing dotfiles..."
 cd && cd dotfiles && stow --ignore="bootstrap.sh" --ignore=".aerospace.toml" --ignore=".config/ghostty/config" --adopt . && git restore .
-
-echo "Setting up basic project structure..."
-cd && cd .. && cd .. && cd workspaces && cd $(ls)
-uv init && uv venv && uv python install 3.14 && uv python update-shell
-mkdir -p src/${PWD##*/}
-mkdir docs
-mkdir data
-mkdir exports
-mv main.py src/${PWD##*/}/main.py
-
-echo "Cleaning home directory..."
-cd && rm -rf .bash_logout .bashrc .oh-my-zsh .zcom*
-
-echo "Installing LSPs, Linters, Formatters with Mason..."
-nvim --headless -c "MasonInstall gopls lua-language-server prettier ruff stylua tinymist ty typstyle"
 
 echo "Setup finished."
