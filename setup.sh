f153de707daf702eb3e34e1281cb7b3063df782a
Szymon Zubek <me@szymonzubek.com>
2026-03-18 21:41:02 +0000

    Added fastfetch and removed Mason auto install
    Signed-off-by: Szymon Zubek <me@szymonzubek.com>

--- setup.sh
+++ setup.sh
@@ -14,6 +14,7 @@ packages=(
   tmux
   zsh-autosuggestions
   zsh-syntax-highlighting
+  fastfetch
 )
 
 for pkg in "${packages[@]}"; do
@@ -26,8 +27,5 @@ brew install neovim --HEAD
 
 echo "Stowing dotfiles..."
 cd && cd dotfiles && stow --ignore="bootstrap.sh" --ignore=".aerospace.toml" --ignore=".config/ghostty/config" --adopt . && git restore .
-
-echo "Installing LSPs, Linters, Formatters with Mason..."
-nvim --headless -c "MasonInstall gopls lua-language-server prettier ruff stylua tinymist ty typstyle"
 
 echo "Setup finished."
