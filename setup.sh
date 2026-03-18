3e62227bd0f962dac47ad01edb1a9e61baf89ba4
Szymon Zubek <me@szymonzubek.com>
2026-03-18 18:48:04 +0000

    Moving all tools to features
    Signed-off-by: Szymon Zubek <me@szymonzubek.com>

--- setup.sh
+++ setup.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 echo "Setup starting..."
 
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-source ~/.zshrc
-
-echo "Brew installing..."
-brew install fzf bat ripgrep stow starship zsh-autosuggestions zsh-syntax-highlighting sk fd fastfetch gcc gh python@3.14 npm
-echo "Brew finished."
-
 echo "Stowing dotfiles..."
 cd && cd dotfiles && stow --ignore="bootstrap.sh" --ignore=".aerospace.toml" --ignore=".config/ghostty/config" --adopt . && git restore .
 
