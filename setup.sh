8b9d57f8dd2b97ecf20650e61c0518e99270b3b7
Szymon Zubek <me@szymonzubek.com>
2026-03-18 14:43:18 +0000

    Fixed homebrew not being in PATH and went back to using Mason
    Signed-off-by: Szymon Zubek <me@szymonzubek.com>

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "Setup starting..."
 
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+source ~/.zshrc
+
 echo "Brew installing..."
 brew install fzf bat ripgrep stow starship zsh-autosuggestions zsh-syntax-highlighting sk fd fastfetch gcc gh
 echo "Brew finished."
@@ -8,7 +11,7 @@ echo "Brew finished."
 echo "Stowing dotfiles..."
 cd && cd dotfiles && stow --ignore="bootstrap.sh" --ignore=".aerospace.toml" --ignore=".config/ghostty/config" --adopt . && git restore .
 
-# echo "Installing LSPs, Linters, Formatters with Mason..."
-# nvim --headless -c "MasonInstall gopls lua-language-server prettier ruff stylua tinymist ty typstyle"
+echo "Installing LSPs, Linters, Formatters with Mason..."
+nvim --headless -c "MasonInstall gopls lua-language-server prettier ruff stylua tinymist ty typstyle"
 
 echo "Setup finished."
